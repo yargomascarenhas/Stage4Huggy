af1386b112360af3412776d9495ebe99f14a2d37
Yargo Mascarenhas <yargomascarenhas@gmail.com>
2018-02-14 17:14:03 -0300

    Ajustando as rotas do angular

--- fronthuggy/src/app/app.module.ts
+++ fronthuggy/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AuthModule } from "./auth/auth.module";
+import { provideRoutes, RouterOutlet } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    AuthModule,
+    RouterOutlet
   ],
   providers: [],
   bootstrap: [AppComponent]
